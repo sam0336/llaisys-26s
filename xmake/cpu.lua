e922933eebfc1f82496c9ef13cc1d24b0718e5f8
guo ang <2073575944@qq.com>
2026-08-05 18:06:20 +0800

    添加 llaisys-models 和 llaisys-models-cpu 编译目标

--- xmake/cpu.lua
+++ xmake/cpu.lua
@@ -25,3 +25,17 @@ target("llaisys-ops-cpu")
     on_install(function (target) end)
 target_end()
 
+target("llaisys-models-cpu")
+    set_kind("static")
+    add_deps("llaisys-ops")
+    set_languages("cxx17")
+    set_warnings("all", "error")
+    if not is_plat("windows") then
+        add_cxflags("-fPIC", "-Wno-unknown-pragmas")
+    end
+
+    add_files("../src/models/qwen2/cpu/*.cpp")
+
+    on_install(function (target) end)
+target_end()
+
