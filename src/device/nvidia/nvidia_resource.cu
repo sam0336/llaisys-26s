2e02012085b229915eb3e88ae90eaf2727747aa3
guo ang <2073575944@qq.com>
2026-08-10 11:20:37 +0800

    nvidia::Resource 添加 cuBLAS handle 生命周期管理

--- src/device/nvidia/nvidia_resource.cu
+++ src/device/nvidia/nvidia_resource.cu
@@ -1,7 +1,24 @@
 #include "nvidia_resource.cuh"
 
+#include <cuda_runtime.h>
+#include <stdexcept>
+
 namespace llaisys::device::nvidia {
 
-Resource::Resource(int device_id) : llaisys::device::DeviceResource(LLAISYS_DEVICE_NVIDIA, device_id) {}
+Resource::Resource(int device_id)
+    : llaisys::device::DeviceResource(LLAISYS_DEVICE_NVIDIA, device_id) {
+    cublasStatus_t stat = cublasCreate(&_cublas_handle);
+    if (stat != CUBLAS_STATUS_SUCCESS) {
+        throw std::runtime_error("Failed to create cuBLAS handle for device "
+                                 + std::to_string(device_id));
+    }
+}
+
+Resource::~Resource() {
+    if (_cublas_handle) {
+        cublasDestroy(_cublas_handle);
+        _cublas_handle = nullptr;
+    }
+}
 
 } // namespace llaisys::device::nvidia
--- src/device/nvidia/nvidia_resource.cuh
+++ src/device/nvidia/nvidia_resource.cuh
@@ -2,10 +2,20 @@
 
 #include "../device_resource.hpp"
 
+#include <cublas_v2.h>
+
 namespace llaisys::device::nvidia {
+
 class Resource : public llaisys::device::DeviceResource {
 public:
     Resource(int device_id);
     ~Resource();
+
+    /** @brief cuBLAS handle for this device. */
+    cublasHandle_t cublas_handle() const { return _cublas_handle; }
+
+private:
+    cublasHandle_t _cublas_handle = nullptr;
 };
+
 } // namespace llaisys::device::nvidia
